ab359db51e2c55464dd6ab70ff4f116bcfc472ee
Gambda-II <alexanderlorengel_tgd@hotmail.de>
2023-09-16 10:12:47 +0200

    Fixed render of all cards, shuffled all card

--- Klondike/Program.cs
+++ Klondike/Program.cs
@@ -10,60 +10,87 @@ string[] renderCard = newCard.Render();
 RenderCard(newCard);
 */
 
+using System.ComponentModel.DataAnnotations;
+
 CreateGame();
 
 void CreateGame()
 {
-    Console.WindowHeight    = 3 * ( Card.renderHeight + 1);
-    Console.WindowWidth     = 7 * (Card.renderWidth + 1);
+    int[] numbers = new int[52];
+    for (int i = 0; i < 52; i++)
+    {
+        numbers[i] = i;
+    }
+
+    Random rnd = new Random();
+    int[] permutation = numbers.OrderBy(x => rnd.Next()).ToArray();
+
+
+
+    Console.WindowHeight    = 3 * ( Card.renderHeight + 5);
+    Console.WindowWidth     = 7 * (Card.renderWidth + 2);
 
     int posX = 0, posY = 0;
-    Card[] cards = new Card[51];
+    Card[] cards = new Card[52];
+    Card[] shuffledCards = new Card[52];
 
-
-    for (int k = 0;k < 3;k++)
+    for (int k = 1;k < 52; k++)
     {
-        cards[k].faceUp = false;
-        cards[k].value = Value.Jack;
-        cards[k].suit = Suit.Spades;
+        int i = (k - 1) / 13;
+
+                cards[k-1] = new Card();
+                cards[k-1].faceUp = true;
+                cards[k-1].value = (k % 13 == 0) ? (Value) 13 : (Value) (k % 13);
+                cards[k-1].suit = (Suit) i;
+
     }
-
-    for (int k = 0;k < 3 * 7 - 1;k++)
+    
+    for (int k = 0; k < 52; k++)
     {
-        if (k == 1 || (k > 7 && k < 15))
+        shuffledCards[k] = cards[permutation[k]];
+    }
+    
+    for (int k = 0; k < 21; k++)
+    {
+        if (k != 1 && k !=2 && !(k > 6 && k < 14))
         {
-
+            RenderCard(shuffledCards[k], (k % 7) * Card.renderWidth, (k / 7) * Card.renderHeight);
         }
-        RenderCard(cards[k], k * Card.renderWidth, k * Card.renderHeight);
+        
 
     }
 
-//  Console.SetCursorPosition(posX, posY);
-/*
-    Card card = new Card();
-    card.faceUp = false;
-    card.value = Value.Jack;
-    card.suit = Suit.Spades;
-    RenderCard(card,posX,posY);
 
-    card.faceUp = true;
-    card.value = Value.Ace;
-    card.suit = Suit.Spades;
-    RenderCard(card, posX + 0 * Card.renderWidth, posY + 1 * Card.renderHeight + 1);
 
-    card.faceUp = true;
-    card.value = Value.Three;
-    card.suit = Suit.Clubs;
-    RenderCard(card, posX + 1 * Card.renderWidth, posY + 1 * Card.renderHeight + 1);
 
-    Console.SetCursorPosition(20, 9);
-    card.faceUp = true;
-    card.value = Value.Four;
-    card.suit = Suit.Hearts;
-    RenderCard(card, posX + 2 * Card.renderWidth, posY + 1 * Card.renderHeight+ 1);
 
-    Console.Read();
-*/
+
+    //  Console.SetCursorPosition(posX, posY);
+    /*
+        Card card = new Card();
+        card.faceUp = false;
+        card.value = Value.Jack;
+        card.suit = Suit.Spades;
+        RenderCard(card,posX,posY);
+
+        card.faceUp = true;
+        card.value = Value.Ace;
+        card.suit = Suit.Spades;
+        RenderCard(card, posX + 0 * Card.renderWidth, posY + 1 * Card.renderHeight + 1);
+
+        card.faceUp = true;
+        card.value = Value.Three;
+        card.suit = Suit.Clubs;
+        RenderCard(card, posX + 1 * Card.renderWidth, posY + 1 * Card.renderHeight + 1);
+
+        Console.SetCursorPosition(20, 9);
+        card.faceUp = true;
+        card.value = Value.Four;
+        card.suit = Suit.Hearts;
+        RenderCard(card, posX + 2 * Card.renderWidth, posY + 1 * Card.renderHeight+ 1);
+
+        Console.Read();
+    */
 }
 
 void RenderCard(Card card,int posX, int posY)
