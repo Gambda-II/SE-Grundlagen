8b16df609cc301581ffc77f5e4bd1afbe8e041b6
Gambda-II <alexanderlorengel_tgd@hotmail.de>
2023-09-18 22:18:03 +0200

    äh

---
+++ dotnet-cli/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Wie viele Zufallszahlen möchtest du?");
+
+var inputAmount = Console.ReadLine();
+var randomAmount = int.Parse(inputAmount);
+var rand = new Random();
+
+int[] randomNumbers = new int[randomAmount];
+
+for (int k = 0; k < randomAmount; k++)
+{
+    randomNumbers[k] = rand.Next(0,10);
+}
+
+foreach(int x in randomNumbers)
+{
+Console.WriteLine(x);
+}
