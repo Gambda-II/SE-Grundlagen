0b55ccbfca532115b9acd784529d18136c36d096
Gambda-II <alexanderlorengel_tgd@hotmail.de>
2023-09-18 22:19:16 +0200

    öh

---
+++ ProjektStruktur/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjektStruktur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            Console.WriteLine((int)Console.ReadKey().KeyChar);
+        }
+
+    }
+}
