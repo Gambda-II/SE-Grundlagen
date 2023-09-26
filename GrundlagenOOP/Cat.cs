2dbc1cd857cc3bbfde19a9d49572fed697c86c71
Gambda-II <144769701+Gambda-II@users.noreply.github.com>
2023-09-26 16:24:36 +0200

    Added GrundlagenOOP and tried some stuff

---
+++ GrundlagenOOP/Cat.cs
@@ -0,0 +1,45 @@
+﻿namespace GrundlagenOOP;
+public class Cat
+{
+    // Variables inside a class -> Properties
+    public string Name;
+    public int Age;
+    public string Color;
+
+    //Constructor
+    public Cat(string name, int age, string color)
+    {
+        Name = name;
+        Age = age;
+        Color = color;
+    }
+
+    public Cat(int Age, string Name, string Color)
+    {
+        this.Age = Age;
+        this.Name = Name;
+        this.Color = Color;
+    }
+
+
+    // Functions inside a class -> Methods
+    public static void Meow()
+    {
+        Console.WriteLine("Meow!");
+    }
+
+    public void Meow(string s = "")
+    {
+        Console.WriteLine("Meow!");
+    }
+
+    public override string ToString()
+    {
+
+        return $"Name: {this.Name}\nAge: {this.Age}\nColor: {this.Color}";
+    }
+}
+
+// static class --> kann keine Instanzen erstellen, kann nicht als Typelement verwendet werden --> statische Methoden werden über die Klasse 
+
+// non static class --> kann Instanzen erstellen, kann als Typelement verwendet werden, Methoden können static oder non static sein
---
+++ GrundlagenOOP/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GrundlagenOOP;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Statische Klasse Console
+        // Console.WriteLine("Hello, World!");
+
+        // Instanziierbare Klasse
+        // Random rnd = new Random();
+
+        // static method by namespace.class.method
+        GrundlagenOOP.Cat.Meow();
+
+        // static method by class.method
+        Cat.Meow();
+
+        // instantiate new object
+        Cat cat = new Cat("", 0, "");
+
+        cat.Meow();
+        cat.Name = "Katze";
+        cat.Age = 99;
+        cat.Color = "transparent";
+
+        Console.WriteLine(cat);
+        Cat kat = new Cat("Katzer", 100, "blau");
+        Console.WriteLine(kat);
+    }
+}
